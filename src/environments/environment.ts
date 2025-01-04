70b659b60540a9fe49c1e843cc04977891622366
Shafiqah24 <2023637684@student.uitm.edu.my>
2025-01-04 15:55:45 +0800

    menjadilah menjadilah

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,11 +1,11 @@
 export const environment = {
   firebase: {
-    apiKey: 'AIzaSyA_Hbk5H2Jc5xoJVOYvHUZe4eFeXuOaGRcEY',
-    authDomain: 'friendlychat-7ef45.firebaseapp.com',
-    projectId: 'friendlychat-7ef45',
-    storageBucket: 'friendlychat-7ef45.firebasestorage.app',
-    messagingSenderId: '550797487520',
-    appId: '1:550797487520:web:4405e4855deb2873cb08c5',
-    measurementId: 'G-DRTY2NMYFM',
+    apiKey: "AIzaSyA_Hbk5H2Jc5xoJVOYvHUZe4eFeXuOaGRc",
+    authDomain: "friendlychat-7ef45.firebaseapp.com",
+    projectId: "friendlychat-7ef45",
+    storageBucket: "friendlychat-7ef45.firebasestorage.app",
+    messagingSenderId: "550797487520",
+    appId: "1:550797487520:web:4405e4855deb2873cb08c5",
+    measurementId: "G-DRTY2NMYFM",
   },
 };
