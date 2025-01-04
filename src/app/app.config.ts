d495d714a05a5eb7b6b95a307aa37263f654290b
Shafiqah24 <2023637684@student.uitm.edu.my>
2025-01-04 14:56:14 +0800

    Please be okay

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -11,8 +11,6 @@ import { provideRouter } from '@angular/router';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    importProvidersFrom(
-    ),
     provideRouter(routes), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-7ef45","appId":"1:550797487520:web:8ea54e3529823a01cb08c5","storageBucket":"friendlychat-7ef45.firebasestorage.app","apiKey":"AIzaSyA_Hbk5H2Jc5xoJVOYvHUZe4eFeXuOaGRc","authDomain":"friendlychat-7ef45.firebaseapp.com","messagingSenderId":"550797487520","measurementId":"G-KC38VW9J0F"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage()), provideFirebaseApp(() => initializeApp({"projectId":"friendlychat-7ef45","appId":"1:550797487520:web:4405e4855deb2873cb08c5","storageBucket":"friendlychat-7ef45.firebasestorage.app","apiKey":"AIzaSyA_Hbk5H2Jc5xoJVOYvHUZe4eFeXuOaGRc","authDomain":"friendlychat-7ef45.firebaseapp.com","messagingSenderId":"550797487520","measurementId":"G-DRTY2NMYFM"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore()), provideMessaging(() => getMessaging()), provideStorage(() => getStorage())
   ],
 };
